d9f654d1ad7602e3b0af17db393599cc42f4533b
kstar937 <112125132+kstar937@users.noreply.github.com>
2024-04-05 21:00:36 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,7 @@
+const SLIDE_URL = "Lien de votre SLIDE";
+// # Latitude de votre ville
+const LATITUDE = 0;
+// # Longitude de votre ville
+const LONGITUDE = 0;
+// # Elevation de votre ville
+const ELEVATION = 0;
